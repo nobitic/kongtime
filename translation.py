d0d73612d0a35398b8b52f98263becd3717cf3d4
nobitic <100597038+nobitic@users.noreply.github.com>
2022-03-06 05:44:45 +0000

    Update translation.py

--- translation.py
+++ translation.py
@@ -11,21 +11,13 @@ class LuciferMoringstar(object):
 
 🧑 My Creator : {dev_name}
 
-📦 My Source : [Click Here](https://github.com/PR0FESS0R-99/LuciferMoringstar_Robot)
+🖇️ My Language : [Python3]
 
-📺 My Tutorial : [Ckick Here](https://youtu.be/FCU_XJmyG_U)
+🗃️ My Database : [Mongodb]
 
-🗃️ My Library : [Pyrogram](https://youtu.be/Af055Eozk9s)
+🗳️ My Server : [Heroku]
 
-🖇️ My Language : [Python3](https://youtu.be/Af055Eozk9s)
-
-🗃️ My Database : [Mongodb](https://youtu.be/gBLTsH-IXr0)
-
-🗳️ My Server : [Heroku](https://youtu.be/xcZD55td6Vw)
-
-🏷️ My Updates : [Mo Tech](t.me/Mo_Tech_YT)
-
-🗣️ My Support : [Any Doubt](t.me/Mo_Tech_Group)"""
+🗣️ My Support : [Any Doubt](t.me/FM_MOVE)"""
 
     FILE_CAPTIONS = """Hello 👋 {mention}\n\n📁Title {title}\n\n🔘Size {size}"""
 
